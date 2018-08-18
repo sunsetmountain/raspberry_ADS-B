a17fda9d541d01252c617439c7788ea7af43d4db
sunsetmountain <brandon@sunsetmountain.com>
2018-08-18 12:04:54 -0600

    Create batchPub.py

---
+++ batchPub.py
@@ -0,0 +1,68 @@
+import os
+import time
+from google.cloud import pubsub
+from oauth2client.service_account import ServiceAccountCredentials
+
+dataDir = "/home/pi/rawData"
+
+def publishBatch(msgList):
+
+	credentialsJson = "/home/pi/[keyfile].json"
+	scopesList = ["https://www.googleapis.com/auth/pubsub"]
+	credentialsObj = ServiceAccountCredentials.from_json_keyfile_name(
+		credentialsJson,
+		scopes = scopesList
+	)
+
+	pubsubClient = pubsub.Client(
+		project="[projectName]"
+	)
+
+	topicName = "raw-adsb-data"
+	topicObj = pubsubClient.topic(topicName)
+        with topicObj.batch() as batchObj:
+                for eachItem in msgList:
+                        eachItem = eachItem.encode("utf-8")
+                        batchObj.publish(eachItem)
+
+	return
+
+
+def loadList(fileList):
+        msgList = []
+        for fileName in fileList:
+                filePath = dataDir + "/" + fileName
+		with open(filePath) as fileObj:
+                        fileContents = fileObj.read()
+                        msgList.append(fileContents)
+
+        return msgList
+
+
+def main():
+        while True:
+                fileList = os.listdir(dataDir)
+        	msgList = loadList(fileList)
+        	print len(msgList)
+
+                try:
+                        if len(msgList) > 0:
+                                publishBatch(msgList)
+                                for fileName in fileList:
+                                        filePath = dataDir + "/" + fileName
+                                        os.remove(filePath)
+                        else:
+                                print "No messages to send..."
+                        time.sleep(2)
+
+                except KeyboardInterrupt:
+                        quit()
+                
+                except Exception,e:
+                        print "Encountered error..."
+                        print e
+                        continue
+
+
+if __name__ == "__main__":
+	main()
